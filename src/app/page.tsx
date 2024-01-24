3918906d73be24d15d9b016402198717aa452dc8
Sundaram Kumar Jha <jhasundaram@outlook.com>
2024-01-24 23:04:40 +0530

    cleaned up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,11 @@
-"use client";
 
-import React, { createContext, useContext, useState } from "react";
-
-interface ContextProps {
-  state: boolean;
-  setState: React.Dispatch<React.SetStateAction<boolean>>;
-}
-
-export const Ctx = createContext<ContextProps>({ state: true, setState: () => {} });
 
 export default function Home() {
-  const [state, setState] = useState<boolean>(true);
-
-  return (
-    <Ctx.Provider value={{ state, setState }}>
-      <Component />
-    </Ctx.Provider>
-  );
-}
-
-const Component = () => {
-  const { state, setState } = useContext(Ctx);
 
   return (
     <div>
-      {state ? <h1>hello</h1> : ""}
-      <button onClick={() => setState(!state)}>Pop</button>
+      Hello
     </div>
   );
-};
+}
+
